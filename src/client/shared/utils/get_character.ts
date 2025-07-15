1c85fad265ddf356e6ad45e2acd1b92028612a36
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2025-07-15 15:40:57 -0700

    refactor: added flamework

--- src/client/shared/utils/get_character.ts
+++ src/client/shared/utils/get_character.ts
@@ -3,5 +3,5 @@ import { Character } from "shared/types/character";
 
 export function getClientCharacter(): Character | undefined {
     const player = Players.LocalPlayer;
-    return (player?.Character || player?.CharacterAdded.Wait()[0]) as Character | undefined;
+    return (player.Character || player.CharacterAdded.Wait()[0]) as Character | undefined;
 }
